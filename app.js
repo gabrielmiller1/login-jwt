c9c76794e7937c5c5aaa0eeacd9e85b7bc4e7c7c
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-08-28 18:47:04 -0300

    Creating User.js

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL,
     },
     (error) => {
         if (error)
-            console.log(error());
+            console.log(error);
         else
             console.log('Mongo Connected');
     });
 
 
-app.use('/user', userRouter);
+app.use('/user', express.json(), userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('Server running');
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,20 @@
+const User = require('../models/User')
+
 const userController = {
 
-    register: function (req, res) {
-        res.send('Register');
+    register: async function (req, res) {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        try{
+            const savedUser = await user.save();
+            res.send(savedUser);
+        } catch (error){
+            res.status(400).send(error);
+        }
     },
 
     login: function (req, res) {
